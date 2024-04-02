ddab873b50315a9f77b36ae256a4cb1bdda44d8d
robertcthibault <104090584+robertcthibault@users.noreply.github.com>
2024-04-02 08:50:31 +0700

    typecasting commit

---
+++ typecasting.py
@@ -0,0 +1,19 @@
+#typecasting == the process of converting a value of one data type to another 
+# works on (string, integer, float, boolean)
+# can be done explicitly or implicitly 
+
+
+#Explict typecasting
+name = "Bro"
+age = 39
+gpa = 3.5
+student = True
+
+
+#find the type 
+type(age)
+print(type(age))
+
+#change the type explicitly 
+age = float(age)
+print(age)
