e39ec79899ac7418312b1e53e64a4f3efb7ce3ef
robertcthibault <104090584+robertcthibault@users.noreply.github.com>
2024-04-02 10:20:14 +0700

    creating folders and files

---
+++ MATH-IS-EASY/simple_operators.py
@@ -0,0 +1,22 @@
+
+friends = 0
+
+#addition
+#friends = friends + 1
+friends += 1 #augmented asignment operator 
+
+#subtraction
+friends -= 2
+
+#multiplication
+friends *= 3
+
+#division
+friends /= 2
+
+#exponents
+friends **= 2
+
+#modulous --> give you the remainder of any division --> find if a number is even or odd
+remainder = friends % 3 #divids friends into groups of 3
+print(friends)
